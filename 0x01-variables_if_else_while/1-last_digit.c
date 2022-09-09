7991ec5761e3d336a52f76f6a6747248e846c83b
Chukwunenye <chunkums088@gmail.com>
2022-09-09 06:49:30 -0700

    Add a program that will assign a random number to the variable n each time it is executed

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+/**
+
+ * Program written by Chukwunenye Moses
+
+ */
+
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print a random number, stating if it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+ */
+
+int main(void) 
+
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n > 0)   
+printf("%d is positive\n", n);
+
+else if (n < 0)
+printf("%d is negative\n", n);
+
+else  
+printf("%d is zero\n", n);
+
+return (0);
+
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This README.md will describe the program run in every file in this directory
