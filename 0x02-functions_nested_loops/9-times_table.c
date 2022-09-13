6d7fe9e4cada26c10449347d6523754d07afdfc8
Chukwunenye <chunkums088@gmail.com>
2022-09-13 09:10:49 -0700

    Remove excessive spacing between lines that made the function cross 40 lines as spotted out by Betty linter

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,48 +9,36 @@
 void times_table(void)
 
 {
-
 int a, b, times;
 
 for (a = 0; a <= 9; a++)
 
 {
 for (b = 0; b <= 9; b++)
-
 {
-
 times = a * b;
 
 if (b != 0)
 {
-
 _putchar(',');
 _putchar(' ');
-
 }
 
 if (b == 0)
-
 {
 _putchar('0');
 }
 
 else if (times >= 10)
-
 {
-
 _putchar((times / 10) + '0');
 _putchar((times % 10) + '0');
-
 }
 
 else if ((times < 10) && (b != 0))
-
 {
-
 _putchar(' ');
 _putchar((times % 10) + '0');
-
 }
 
 }
