fdc69d1cc907a0c9f59170ea8b6a811bbf32908e
Chukwunenye <chunkums088@gmail.com>
2022-09-15 18:46:13 -0700

    Change the code in this file

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,32 +3,36 @@
 /**
  * print_triangle - prints a triangle
  *@size: size of the triangle
- *
- * Return: Always 0.
  */
 
 void print_triangle(int size)
 
 {
 
-int x;
-
-int y;
-
-for (x = 1; x <= size; x++)
+if (size <= 0)
 
 {
-
-for (y = 1; y <= size; y++)
-{
-if (y <= size - x)
-_putchar(' ');
+_putchar('\n');
+}
 else
+{
+int i, j;
+
+for (i = 1; i <= size; i++)
+{
+for (j = i; j < size; j++)
+{
+_putchar(' ');
+}
+
+for (j = 1; j <= i; j++)
+{
 _putchar('#');
 }
 
 _putchar('\n');
 
+}
 }
 
 }
