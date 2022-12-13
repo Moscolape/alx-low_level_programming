d3c5a1e5e3f2a06989defcec969ccacde8f7b34d
Chukwunenye Moses <chunkums088@gmail.com>
2022-12-13 02:04:54 +0100

    Update 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
-gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -c -fpic *.c
+gcc -shared *.o -o liball.so
